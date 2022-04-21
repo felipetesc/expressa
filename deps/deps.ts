2bfd89a4d98b2c592287c9524c5f1a8cf327a5ed
felipetesc <felipetesc@gmail.com>
2022-04-21 10:29:57 -0300

    changed imports

---
+++ deps/deps.ts
@@ -0,0 +1,5 @@
+
+import { serve } from "https://deno.land/std@0.135.0/http/server.ts";
+import { parse, format } from "https://deno.land/std@0.135.0/datetime/mod.ts";
+
+export {serve, parse, format};
--- runner/runner.ts
+++ runner/runner.ts
@@ -1,6 +1,6 @@
 
-import { serve } from "https://deno.land/std@0.135.0/http/server.ts";
-import { parse, format } from "https://deno.land/std@0.135.0/datetime/mod.ts";
+import { serve, parse, format } from "../deps/deps.ts";
+
 import { AppContext , DefaultErrHandler } from "../handlers/handlers.ts";
 
 class Expressa{
